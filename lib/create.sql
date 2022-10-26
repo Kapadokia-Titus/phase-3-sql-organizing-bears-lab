51c693efb6eb3e98f317bd9e1df521f1938d2ac9
kapadokia <ktnyandoch@gmail.com>
2022-10-26 22:50:08 +0300

    sql fully exploited

---
+++ lib/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE bears( 
+                    id INTEGER PRIMARY KEY, 
+                    name TEXT, 
+                    age INT NOT NULL, 
+                    sex TEXT NOT NULL, 
+                    color TEXT NOT NULL, 
+                    temperament TEXT NOT NULL, 
+                    alive INT NOT NULL) 
